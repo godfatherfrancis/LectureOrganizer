58a5cc47e444d0dbad37975b8ea70175e4f03d58
Corey Shaw <coreyrshaw@gmail.com>
2016-06-09 19:31:19 -0400

    First commit

---
+++ src/main/java/tiy/lecture/organizer/LectureOrganizerApplication.java
@@ -0,0 +1,12 @@
+package tiy.lecture.organizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureOrganizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LectureOrganizerApplication.class, args);
+	}
+}
